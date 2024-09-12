026f8a1fa9102eab4c4f8b650f46a645b2391eed
Samuel Joli <samuel.joli.ftn@gmail.com>
2024-09-11 21:53:31 -0400

    feat(neovim): show diagnostics line in float

--- home-manager/neovim/lsp.lua
+++ home-manager/neovim/lsp.lua
@@ -51,6 +51,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		--  Useful when your language has ways of declaring types without an actual implementation.
 		map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
+		-- Show diagnostic on line in float
+		map("gl", function()
+			vim.diagnostic.open_float()
+		end, "Hover diagnostics")
+
 		-- Jump to the type of the word under your cursor.
 		--  Useful when you're not sure what type a variable is and you want to see
 		--  the definition of its *type*, not where it was *defined*.
